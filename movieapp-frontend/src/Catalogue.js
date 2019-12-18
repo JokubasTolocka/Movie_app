befb5185842bc064337594771417855c1b40ea1b
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-12-18 14:26:28 +0200

    app fetches reviews, no redux

--- movieapp-frontend/src/Catalogue.js
+++ movieapp-frontend/src/Catalogue.js
@@ -1,9 +1,33 @@
 import React, {Component} from 'react';
 
 class Catalogue extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            reviews: []
+            
+        }
+    }
+    componentWillMount(){
+        this.getReviews();
+    }
+    getReviews(){
+        fetch('http://localhost:8000')
+            .then(res => {
+                return res.json();
+            })
+            .then(posts => {
+                this.setState({reviews: posts})
+            })
+            .catch(err => console.log(err))
+    }
+
     render(){
         return(
-            <h1>Hi</h1>
+            <div>
+                <h1>Hi</h1>
+                
+            </div>
         );
     }
 }
