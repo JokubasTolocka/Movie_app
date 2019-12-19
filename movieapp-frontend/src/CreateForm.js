05ed203874d6c123dd378a35ca62963fbd11dade
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-12-19 15:21:47 +0200

    create input form, state catches inputs, no post request

--- movieapp-frontend/src/CreateForm.js
+++ movieapp-frontend/src/CreateForm.js
@@ -1,9 +1,79 @@
 import React, {Component} from 'react';
 
 class CreateForm extends Component {
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            title: '',
+            user: '',
+            imageUrl: '',
+            review: ''
+        }
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("got to the beginning")
+        
+
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
     render(){
         return(
-            <h1>Create Form</h1>
+            <div className='create-container'>
+                <div className='create-review'>
+                    <h2>Your Review:</h2>
+                    <form onSubmit={this.handleSubmit} className='create-form'>
+                        <label htmlFor='movie'/>
+                        <input
+                            className='create-input'
+                            placeholder='Movie Title'
+                            name='title'
+                            onChange={this.handleChange}
+                            type='text'
+                        />
+                        <label htmlFor='creator'/>
+                        <input
+                            className='create-input'
+                            placeholder='Your Name'
+                            name='user'
+                            onChange={this.handleChange}
+                            type='text'
+                        />
+                        <label htmlFor='image'/>
+                        <input
+                            className='create-input'
+                            placeholder='URL for the Image of that Movie'
+                            name='imageUrl'
+                            onChange={this.handleChange}
+                            type='text'
+                        />
+                        <label htmlFor='review'/>
+                        <input 
+                            className='create-review-text'
+                            placeholder='Your Review'
+                            name='review'
+                            onChange={this.handleChange}
+                            type='text'
+                        />
+                        <button
+                            className='create-button'
+                            type='submit'
+                        >Submit!</button>
+                    </form>
+                </div>
+                    <h2 >Preview</h2>
+                <div className='create-preview'>
+
+                </div>
+            </div>
         );
     }
 }
