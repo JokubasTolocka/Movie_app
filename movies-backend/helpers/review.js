e89358b7e18956789ccf779dec8a7ffae95999ad
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-12-25 17:28:31 +0200

    add update and delete functions

--- movies-backend/helpers/review.js
+++ movies-backend/helpers/review.js
@@ -1,16 +1,24 @@
 const db = require('../models');
 
 exports.createReview = async function(req,res,next){
-    try{
-        let review = await db.Review.create(req.body);
-        console.log(req.body);
-        return res.status(200).json(review);
+    try {
+        let review = await db.Review.create({
+            image: req.body.image,
+            title: req.body.title,
+            text: req.body.text,
+            user: req.params.id
+        });
+        let foundUser = await db.User.findById(req.params.id);
+        foundUser.reviews.push(review.id);
+        await foundUser.save();
+        let foundReview = await db.Review.findById(review._id).populate('user', {
+            username: true
+        });
+        return res.status(200).json(foundReview);
     } catch(err){
-        console.log(req.body);
-        console.log("createReview");
         return next(err);
     }
-}
+};
 
 exports.getReview = async function(req,res,next){
     try{
@@ -18,5 +26,40 @@ exports.getReview = async function(req,res,next){
         return res.status(200).json(foundReview);
     } catch(err){
         return next(err);
+    }
+}
+
+exports.deleteReview = async function (req, res, next) {
+    try {
+      let foundReview = await db.Review.findById(req.params.review_id);
+      // let foundMessage = await db.Message.findByIdRemove(req.params.message_id);
+      await foundReview.remove();
+  
+      return res.status(200).json(foundReview);
+    } catch (err) {
+      return next(err);
+    }
+};
+
+exports.updateReview = async function(req, res, next) {
+    try {
+        let updatedReview = await db.Review.findByIdAndUpdate(
+        req.params.message_id,
+        {
+            text : req.body.text,
+            title: req.body.title,
+            image: req.body.image
+        }
+        );
+        await updatedReview.save();
+        // return res.status(200).json(updatedMessage);
+
+        if(res.statusCode >= 200 && res.statusCode <= 300 ) {
+        return res.status(200).send("updated successfully");
+        } else {
+        return next(error);
+        }
+    } catch (error) {
+        return next(error);
     }
 }
