ad4538153a750d67bf26a62744ada0c2fc5f943e
Jokūbas Toločka <jokubas.tj@gmail.com>
2020-01-08 14:24:03 +0200

    Updates

---
+++ precache-manifest.2feb18f940ee4b694671f9f1589c561e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eff29cd0e10ea8f24ffb749a6f1a6b4",
+    "url": "/Movie_app/index.html"
+  },
+  {
+    "revision": "72d70bf61d6c1dd351e5",
+    "url": "/Movie_app/static/css/main.ab3bd847.chunk.css"
+  },
+  {
+    "revision": "4c552685e605188e0fbf",
+    "url": "/Movie_app/static/js/2.363e18cf.chunk.js"
+  },
+  {
+    "revision": "5ec987632bc308bcd1222d7283dbdad9",
+    "url": "/Movie_app/static/js/2.363e18cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72d70bf61d6c1dd351e5",
+    "url": "/Movie_app/static/js/main.6c62de36.chunk.js"
+  },
+  {
+    "revision": "fd57de9452017737212a",
+    "url": "/Movie_app/static/js/runtime-main.4858bfe7.js"
+  },
+  {
+    "revision": "5bbff702933448817705e6d96e0f2858",
+    "url": "/Movie_app/static/media/movieapplogo.5bbff702.png"
+  }
+]);
